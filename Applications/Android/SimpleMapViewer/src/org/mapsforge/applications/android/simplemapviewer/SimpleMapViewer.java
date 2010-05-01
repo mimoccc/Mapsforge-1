c03a598b18e552130c4ee324a016ff4a06c190f9
muehlber@inf.fu-berlin.de <muehlber@inf.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2010-05-01 10:13:57 +0000

    moved SimpleMapViewer.java into the subpackage ".simplemapviewer" to avoid name collisions with other android applications
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@18 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- Applications/Android/SimpleMapViewer/src/org/mapsforge/applications/android/simplemapviewer/SimpleMapViewer.java
+++ Applications/Android/SimpleMapViewer/src/org/mapsforge/applications/android/simplemapviewer/SimpleMapViewer.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.mapsforge.applications.android;
+package org.mapsforge.applications.android.simplemapviewer;
 
 import org.mapsforge.android.map.MapActivity;
 import org.mapsforge.android.map.MapView;
