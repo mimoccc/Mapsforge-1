69e02346d96cc91bd7a7ff88eb843bff08f9b6e2
muehlberg@inf.fu-berlin.de <muehlberg@inf.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2010-06-02 10:11:31 +0000

    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@131 a424f762-2dd8-11df-bfeb-7b4c664f286a

---
+++ Applications/Android/DemoMapViewer/src/org/mapsforge/applications/android/demomapviewer/BasicMapViewer.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2010 mapsforge.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mapsforge.applications.android.demomapviewer;
+
+import org.mapsforge.android.map.MapActivity;
+import org.mapsforge.android.map.MapView;
+
+import android.os.Bundle;
+
+/**
+ * A simple application which demonstrates how to use the MapView.
+ */
+public class BasicMapViewer extends MapActivity {
+	private MapView mapView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.mapView = new MapView(this);
+		this.mapView.setClickable(true);
+		this.mapView.setBuiltInZoomControls(true);
+		this.mapView.setMapFile("/sdcard/berlin.map");
+		setContentView(this.mapView);
+	}
+}
