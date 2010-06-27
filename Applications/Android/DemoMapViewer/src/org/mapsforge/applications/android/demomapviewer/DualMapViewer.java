3ec4c094a659783de8e3ed5015809a35a87982bf
muehlberg@inf.fu-berlin.de <muehlberg@inf.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2010-06-27 09:42:51 +0000

    DualMapViewer no longer sets cache size to zero for each MapView, as bug #22 has been fixed.
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@213 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- Applications/Android/DemoMapViewer/src/org/mapsforge/applications/android/demomapviewer/DualMapViewer.java
+++ Applications/Android/DemoMapViewer/src/org/mapsforge/applications/android/demomapviewer/DualMapViewer.java
@@ -63,14 +63,12 @@ public class DualMapViewer extends MapActivity {
 		this.mapView1 = new MapView(this, MapViewMode.TILE_DOWNLOAD);
 		this.mapView1.setClickable(true);
 		this.mapView1.setBuiltInZoomControls(true);
-		this.mapView1.setFileCacheSize(0);
 		this.mapView1.setMoveSpeed(3);
 
 		this.mapView2 = new MapView(this, MapViewMode.CANVAS_RENDERER);
 		this.mapView2.setClickable(true);
 		this.mapView2.setBuiltInZoomControls(true);
 		this.mapView2.setMapFile("/sdcard/berlin.map");
-		this.mapView2.setFileCacheSize(0);
 		this.mapView2.setMoveSpeed(3);
 
 		// create a LineaLayout that holds both MapViews
