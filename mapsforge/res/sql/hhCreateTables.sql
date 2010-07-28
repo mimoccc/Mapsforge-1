e70865c5b3fc3a6e14e1d37f35c5a43d5cbdee3d
viernau@mi.fu-berlin.de <viernau@mi.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2010-07-28 17:00:55 +0000

    implemented plpgsql function : array_length(DOUBLE PRECISION,INTEGER) which was added to postgres in version 8.4. To use the hh-code with postgres versions < 8.4, the array_length function must defined. a bit slow but does its work^^
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@309 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- mapsforge/res/sql/hhCreateTables.sql
+++ mapsforge/res/sql/hhCreateTables.sql
@@ -77,3 +77,25 @@ CREATE TABLE hh_lvl_stats
 	CONSTRAINT pk_ls PRIMARY KEY (lvl),
 	CONSTRAINT chk CHECK (lvl >= 0)  
 );
+
+/* DIRTY POSTGRES 8.3.? WORKAROUND FOR THE MISSING FUCNTION 'array_length'
+
+CREATE OR REPLACE FUNCTION array_length(arr DOUBLE PRECISION[], dimension INTEGER)
+  RETURNS INTEGER AS $$
+DECLARE
+	ret INTEGER;
+	ad TEXT;
+BEGIN
+	SELECT INTO ad array_dims(arr);
+	SELECT INTO ret substr( ad , strpos(ad,':') + 1 , char_length(ad) - strpos(ad,':') - 1)::integer;
+	RETURN ret;
+END;
+$$ LANGUAGE 'plpgsql';
+
+*/
+
+
+
+
+
+*/
