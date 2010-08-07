cbe89733acc61f7137d54823fe85be6c0a173c8b
thilo.muehlberg@gmail.com <thilo.muehlberg@gmail.com@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2010-08-07 13:12:18 +0000

    added a separate "test" source folder for JUnit tests and moved existing tests there.
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@368 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- mapsforge/test/org/mapsforge/core/GeoCoordinateTest.java
+++ mapsforge/test/org/mapsforge/core/GeoCoordinateTest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.mapsforge.core.tests;
+package org.mapsforge.core;
 
 import static org.junit.Assert.assertTrue;
 
