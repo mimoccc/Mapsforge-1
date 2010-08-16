26319f8fecb288ec0da1550b15e22ae776ae0d9a
eike.send <eike.send@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2010-08-16 18:52:35 +0000

    tests for turn by turn directions
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@420 a424f762-2dd8-11df-bfeb-7b4c664f286a

---
+++ mapsforge/test/org/mapsforge/directions/DummyEdge.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2010 mapsforge.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mapsforge.directions;
+
+import org.mapsforge.core.GeoCoordinate;
+import org.mapsforge.server.routing.IEdge;
+import org.mapsforge.server.routing.IVertex;
+
+/**
+ * This Class is for testing purposes only.
+ * 
+ * @author Eike
+ */
+public class DummyEdge implements IEdge {
+	GeoCoordinate[] wayPoints;
+	int id;
+	String name;
+
+	/**
+	 * @param wayPoints
+	 * @param name
+	 */
+	public DummyEdge(GeoCoordinate[] wayPoints, String name) {
+		this.wayPoints = wayPoints;
+		this.name = name;
+	}
+
+	@Override
+	public GeoCoordinate[] getAllWaypoints() {
+		return wayPoints;
+	}
+
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public IVertex getSource() {
+		return null;
+	}
+
+	@Override
+	public IVertex getTarget() {
+		return null;
+	}
+
+	@Override
+	public GeoCoordinate[] getWaypoints() {
+		return wayPoints;
+	}
+
+	@Override
+	public int getWeight() {
+		return 0;
+	}
+
+}
