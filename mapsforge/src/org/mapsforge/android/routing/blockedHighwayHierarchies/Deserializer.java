2636ad8bc798424d82b3674d70dbe8eda2c87dfb
viernau@mi.fu-berlin.de <viernau@mi.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2010-09-28 22:52:33 +0000

    fixed bug in deserializer
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@551 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- mapsforge/src/org/mapsforge/android/routing/blockedHighwayHierarchies/Deserializer.java
+++ mapsforge/src/org/mapsforge/android/routing/blockedHighwayHierarchies/Deserializer.java
@@ -45,7 +45,8 @@ final class Deserializer {
 	0x00000fff, 0x000007ff, 0x000003ff, 0x000001ff, /** */
 	0x000000ff, 0x0000007f, 0x0000003f, 0x0000001f, /** */
 	0x0000000f, 0x00000007, 0x00000003, 0x00000001, /** */
-	};
+	0x00000000
+			};
 
 	/**
 	 * Reads a bit from the given array
