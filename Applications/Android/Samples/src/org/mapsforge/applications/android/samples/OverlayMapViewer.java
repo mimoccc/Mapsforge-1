25c06abc2000afbe1c3c87c2b747f3a0a8ef8215
muehlberg@inf.fu-berlin.de <muehlberg@inf.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2011-02-12 19:13:19 +0000

    improved the overlay sample application
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@815 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- Applications/Android/Samples/src/org/mapsforge/applications/android/samples/OverlayMapViewer.java
+++ Applications/Android/Samples/src/org/mapsforge/applications/android/samples/OverlayMapViewer.java
@@ -25,6 +25,7 @@ import org.mapsforge.android.maps.OverlayItem;
 import org.mapsforge.android.maps.RouteOverlay;
 
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -43,47 +44,56 @@ public class OverlayMapViewer extends MapActivity {
 		setContentView(mapView);
 
 		// create some points to be shown on top of the map
-		GeoPoint geoPoint1 = new GeoPoint(52.514446, 13.350150); // Victory Column
+		GeoPoint geoPoint1 = new GeoPoint(52.514446, 13.350150); // Berlin Victory Column
 		GeoPoint geoPoint2 = new GeoPoint(52.516272, 13.377722); // Brandenburg Gate
 		GeoPoint geoPoint3 = new GeoPoint(52.525, 13.369444); // Berlin Central Station
 
 		// create the paint objects for the CircleOverlay and set all parameters
 		Paint circleFillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		circleFillPaint.setStyle(Paint.Style.FILL);
-		circleFillPaint.setColor(Color.CYAN);
+		circleFillPaint.setColor(Color.BLUE);
 		circleFillPaint.setAlpha(64);
 
 		Paint circleOutlinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		circleOutlinePaint.setStyle(Paint.Style.STROKE);
-		circleOutlinePaint.setColor(Color.CYAN);
+		circleOutlinePaint.setColor(Color.BLUE);
 		circleOutlinePaint.setAlpha(128);
 		circleOutlinePaint.setStrokeWidth(3);
 
 		// create the CircleOverlay and set the parameters
 		CircleOverlay circleOverlay = new CircleOverlay(circleFillPaint, circleOutlinePaint);
-		circleOverlay.setCircleData(geoPoint3, 75);
+		circleOverlay.setCircleData(geoPoint3, 250);
 
-		// create the paint object for the RouteOverlay and set all parameters
-		Paint routePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		routePaint.setStyle(Paint.Style.STROKE);
-		routePaint.setColor(Color.BLUE);
-		routePaint.setAlpha(160);
-		routePaint.setStrokeWidth(6);
-		routePaint.setStrokeCap(Paint.Cap.ROUND);
-		routePaint.setStrokeJoin(Paint.Join.ROUND);
+		// create the paint objects for the RouteOverlay and set all parameters
+		Paint routePaint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+		routePaint1.setStyle(Paint.Style.STROKE);
+		routePaint1.setColor(Color.BLUE);
+		routePaint1.setAlpha(160);
+		routePaint1.setStrokeWidth(7);
+		routePaint1.setStrokeCap(Paint.Cap.BUTT);
+		routePaint1.setStrokeJoin(Paint.Join.ROUND);
+		routePaint1.setPathEffect(new DashPathEffect(new float[] { 20, 20 }, 0));
+
+		Paint routePaint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+		routePaint2.setStyle(Paint.Style.STROKE);
+		routePaint2.setColor(Color.BLUE);
+		routePaint2.setAlpha(96);
+		routePaint2.setStrokeWidth(7);
+		routePaint2.setStrokeCap(Paint.Cap.BUTT);
+		routePaint2.setStrokeJoin(Paint.Join.ROUND);
 
 		// create the RouteOverlay and set the way nodes
-		RouteOverlay routeOverlay = new RouteOverlay(routePaint, null);
+		RouteOverlay routeOverlay = new RouteOverlay(routePaint1, routePaint2);
 		routeOverlay.setRouteData(new GeoPoint[] { geoPoint1, geoPoint2 });
 
 		// create the ItemizedOverlay with a default marker and set the items
 		Drawable defaultMarker = getResources().getDrawable(R.drawable.marker);
 		ArrayItemizedOverlay itemizedOverlay = new ArrayItemizedOverlay(defaultMarker, this);
 
-		OverlayItem item1 = new OverlayItem(geoPoint1, "Victory Column",
-				"a major tourist attraction");
+		OverlayItem item1 = new OverlayItem(geoPoint1, "Berlin Victory Column",
+				"The Victory Column is a monument in Berlin, Germany.");
 		OverlayItem item2 = new OverlayItem(geoPoint2, "Brandenburg Gate",
-				"one of the main symbols of Berlin");
+				"The Brandenburg Gate is one of the main symbols of Berlin and Germany.");
 
 		itemizedOverlay.addOverlay(item1);
 		itemizedOverlay.addOverlay(item2);
