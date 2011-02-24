df72e45d5acc86e0991667d5c845c5b745084b5b
viernau@mi.fu-berlin.de <viernau@mi.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2011-02-24 10:19:18 +0000

    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@844 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- mapsforge/test/org/mapsforge/preprocessing/highwayHierarchies/mobile/SerializerTest.java
+++ mapsforge/test/org/mapsforge/preprocessing/highwayHierarchies/mobile/SerializerTest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.mapsforge.preprocessing.routing.blockedHighwayHierarchies;
+package org.mapsforge.preprocessing.highwayHierarchies.mobile;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,6 +22,7 @@ import java.util.Random;
 import java.util.Stack;
 
 import org.junit.Test;
+import org.mapsforge.preprocessing.highwayHierarchies.mobile.Serializer;
 
 /**
  * JUnit tests for the BitSerializer
