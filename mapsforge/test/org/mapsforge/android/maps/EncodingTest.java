daf4a1c67458ee415112c5a5902d3d4e9a8d0d2d
jjbross <jjbross@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2011-05-17 21:21:29 +0000

    * pretty-print of header in Java source code files
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@1057 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- mapsforge/test/org/mapsforge/android/maps/EncodingTest.java
+++ mapsforge/test/org/mapsforge/android/maps/EncodingTest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2010, 2011 mapsforge.org
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.mapsforge.android.maps;
 
 import org.mapsforge.preprocessing.map.osmosis.Serializer;
@@ -247,8 +261,8 @@ class TestVBSEncoding extends AsyncTester {
 	/**
 	 * Converts a variable amount of bytes to a signed int.
 	 * <p>
-	 * The first bit is for continuation info, the other six (last byte) or seven (all other
-	 * bytes) bits for data. The second bit in the last byte indicates the sign of the number.
+	 * The first bit is for continuation info, the other six (last byte) or seven (all other bytes) bits
+	 * for data. The second bit in the last byte indicates the sign of the number.
 	 * 
 	 * @return the int value.
 	 */
