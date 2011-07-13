57f7087edc43e722ea53069e3d210b247ea3be6c
Brotlee@zedat.fu-berlin.de <Brotlee@zedat.fu-berlin.de@a424f762-2dd8-11df-bfeb-7b4c664f286a>
2011-07-13 14:56:20 +0000

    PCAdvancedMapViewer - implemented save/load preferences
    git-svn-id: http://mapsforge.googlecode.com/svn/trunk@1264 a424f762-2dd8-11df-bfeb-7b4c664f286a

--- Applications/PC/PortAdvancedMapViewer/src/org/mapsforge/applications/pc/advancedmapviewer/AdvancedMapViewerPC.java
+++ Applications/PC/PortAdvancedMapViewer/src/org/mapsforge/applications/pc/advancedmapviewer/AdvancedMapViewerPC.java
@@ -1,21 +1,13 @@
 package org.mapsforge.applications.pc.advancedmapviewer;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
-
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -27,27 +19,39 @@ import org.mapsforge.core.graphics.Canvas;
 public class AdvancedMapViewerPC extends JFrame implements WindowListener {
 
 	JFrame jFrame;
-	protected Properties properties;
+	protected Properties propertiesStrings, propertiesSettings;
 	private Canvas canvas;
 	private MenuBar menuBar;
 	
 	
 	/** Constructor */
 	public AdvancedMapViewerPC() {
+
+		//Properties
+		try{
+			this.propertiesSettings = new Properties();
+			this.propertiesSettings.load(new FileReader("res/config/config.properties"));
+			this.propertiesStrings = new Properties();
+			this.propertiesStrings.load(new FileReader("res/values/strings.properties"));
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, "Could not read properties files!");
+		}
 		
 		this.addWindowListener(this);
-		this.setTitle("Advanced Map Viewer");
+		this.setTitle(propertiesStrings.getProperty("application_name"));
 		
 		// JFrame
 		this.jFrame = this;
+
+		// Size
+		int height = Integer.parseInt(propertiesSettings.getProperty("application_size_height"));
+		int width = Integer.parseInt(propertiesSettings.getProperty("application_size_width"));
+		this.setSize(height, width);
 		
-		//Properties
-		try{
-			this.properties = new Properties();
-			this.properties.load(new FileReader("res/values/strings.properties"));
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(this, "Could not read strings.properties!");
-		}
+		//Position
+		int xPos = Integer.parseInt(propertiesSettings.getProperty("application_position_x"));
+		int yPos = Integer.parseInt(propertiesSettings.getProperty("application_position_y"));
+		this.setLocation(xPos, yPos);
 		
 		// Menubar
 		this.menuBar = new MenuBar(this);
@@ -63,8 +67,6 @@ public class AdvancedMapViewerPC extends JFrame implements WindowListener {
 	 */
 	public static void main(String[] args) {
 		AdvancedMapViewerPC mainFrame = new AdvancedMapViewerPC();
-		mainFrame.setSize(800, 600);
-		mainFrame.setLocation(200,100);
 		mainFrame.setVisible(true);
 	}
 	
@@ -80,6 +82,11 @@ public class AdvancedMapViewerPC extends JFrame implements WindowListener {
 	/** Cleaning up before exit */
 	protected void close() {
 		//TODO: Cleaning up
+		try {
+			propertiesSettings.store(new FileWriter("res/config/config.properties"), null);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(this, "Could not write to config.properties!");
+		}
 		System.exit(0);
 	}
 
@@ -135,19 +142,18 @@ public class AdvancedMapViewerPC extends JFrame implements WindowListener {
 		//startActivityForResult(new Intent(this, FilePicker.class), SELECT_MAP_FILE);
 	}
 	
-	/** Preferences */
-	protected void startPreferences() {
-		JInternalFrame preferencesPane = new JInternalFrame(properties.getProperty("menu_preferences"), true, true, true, false);
-		EditPreferencesPC editPanel = new EditPreferencesPC();
-		preferencesPane.add(editPanel);
-		preferencesPane.setVisible(true);
-		this.add(preferencesPane);
+	/** Returns the strings properties from the mainFrame. 
+	 * @return the propertiesStrings 
+	 */
+	public Properties getPropertiesStrings() {
+		return propertiesStrings;
 	}
 	
-	/** Returns the properties from the mainFrame. 
-	 * @return the properties 
+	/** Returns the settings properties from the mainFrame. 
+	 * @return the propertiesSettings 
 	 */
-	public Properties getProperties() {
-		return properties;
+	public Properties getPropertiesSettings() {
+		return propertiesSettings;
 	}
+	
 }
